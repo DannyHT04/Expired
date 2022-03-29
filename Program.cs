1f1531e6aaaa1762ea6781404d0ab35e7242cee0
trentbarboza <hellajawns@gmail.com>
2022-03-29 16:04:34 -0700

    added group service

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddScoped <UserService>();
 builder.Services.AddScoped <ItemService>();
+builder.Services.AddScoped <GroupService>();
 
 var connectionString = builder.Configuration.GetConnectionString("ExpiredString");
 builder.Services.AddDbContext<DataContext>(option => option.UseSqlServer(connectionString));
