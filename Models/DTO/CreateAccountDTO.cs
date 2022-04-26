8dbe75b3b9f058fae3358832c2e1974bcd7d933d
DannyHT04 <dannyhat1@gmail.com>
2022-04-26 09:34:22 -0700

    updated usermodel and create account dto

--- Models/DTO/CreateAccountDTO.cs
+++ Models/DTO/CreateAccountDTO.cs
@@ -10,5 +10,9 @@ namespace Expired.Models.DTO
         public int Id { get; set; }
         public string? Username { get; set; }
         public string? Password { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+
     }
 }
