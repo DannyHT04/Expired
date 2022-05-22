eb1639f488a9f75cf239737d5da57c38324bc643
DannyHT04 <dannyhat1@gmail.com>
2022-05-21 21:06:37 -0700

    mirgation

---
+++ Migrations/20220522040405_updatedTableForEndPoints.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Expired.Migrations
+{
+    public partial class updatedTableForEndPoints : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "GroupsInfo",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "UserNameInGroup",
+                value: "Danny,Jovann,Trent,");
+
+            migrationBuilder.UpdateData(
+                table: "GroupsInfo",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "UserNameInGroup",
+                value: "Danny,Jovann,");
+
+            migrationBuilder.UpdateData(
+                table: "GroupsInfo",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "UserNameInGroup",
+                value: "Trent,");
+
+            migrationBuilder.UpdateData(
+                table: "ItemInfo",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "isGroceryList",
+                value: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "GroupsInfo",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "UserNameInGroup",
+                value: "Danny,Jovann,Trent");
+
+            migrationBuilder.UpdateData(
+                table: "GroupsInfo",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "UserNameInGroup",
+                value: "Danny,Jovann");
+
+            migrationBuilder.UpdateData(
+                table: "GroupsInfo",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "UserNameInGroup",
+                value: "Trent");
+
+            migrationBuilder.UpdateData(
+                table: "ItemInfo",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "isGroceryList",
+                value: false);
+        }
+    }
+}
